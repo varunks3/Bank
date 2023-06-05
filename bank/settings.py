b88de69563503065c3016a355bbe81d2cea3c02a
unknown <you@example.com>
2023-06-06 01:28:17 +0530

    fou

--- bank/settings.py
+++ bank/settings.py
@@ -1,3 +1,4 @@
+import os
 from django.conf import settings
 from django.db import connection
 from pathlib import Path
@@ -112,9 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
