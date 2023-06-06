58e2389945ca0013d4cafe6a173c0dbc39f8ea0f
unknown <you@example.com>
2023-06-06 17:11:00 +0530

    phostedss

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "Building the project"
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "make migrations"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "collect static"
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
