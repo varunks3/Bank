3e290567611efb634c5606925343dc0beb968300
unknown <you@example.com>
2023-06-06 19:24:07 +0530

    ffbx

--- bank/settings.py
+++ bank/settings.py
@@ -96,8 +96,7 @@ DATABASES = {
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+
     }
 }
 
