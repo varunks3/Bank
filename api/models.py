88e18b3e0a8cebbf85dddffc35ca3df7a84add0d
unknown <you@example.com>
2023-06-06 16:25:45 +0530

    post

--- api/models.py
+++ api/models.py
@@ -4,16 +4,18 @@ from django.db import models
 class Banks(models.Model):
     name = models.CharField(max_length=49)
     id = models.BigAutoField(primary_key=True)
-
+    def __str__(self):
+        return str(self.id)
 
 class Branches(models.Model):
     ifsc = models.CharField(max_length=11)
-    bank_id = models.BigIntegerField()
+    bank_id = models.ForeignKey(Banks, on_delete=models.CASCADE)
     branch = models.CharField(max_length=74)
     address = models.CharField(max_length=195)
     city = models.CharField(max_length=50)
     district = models.CharField(max_length=50)
     state = models.CharField(max_length=26)
+    
 
 
 class BankBranches(models.Model):
